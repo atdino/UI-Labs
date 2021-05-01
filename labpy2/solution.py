e2e88b168d357f4f44be0e03e02b90de9be56189
Ajdin Trejic <ajdintrejic@protonmail.ch>
2021-05-01 17:55:46 +0200

    solution updejt

--- labpy2/solution.py
+++ labpy2/solution.py
@@ -35,17 +35,16 @@ def load_file_lines(file_path):
     return file_lines
 
 
-def select_clauses(clauses: set, new_clauses: set):
+def select_clauses(clauses: set, new_clause: str):
     #Uklanjanje redudantnih klauzula
     
     #Uklanjanje Nevazecih klauzula
 
     #Odaberi skup parova
-    print(clauses, new_clauses)    
+    print(clauses, new_clause)    
     result = set()
     for clause in clauses:
-        for new_clause in new_clauses:
-            result.add((clause, new_clause))
+        result.add((clause, new_clause))
     return result
 
 def is_negated(clause: str):
@@ -60,37 +59,61 @@ def is_tauntology(c1: str, c2:str):
     else:
         return False
 
+def print_conclusion(conclusion: bool, clause: str):
+    if conclusion:
+        print('[CONCLUSION]: ' + clause +' is true')
+    else:
+        print('[CONCLUSION]: ' + clause +' is false')
+
 def pl_resolve(c1: str, c2: str):
-    c1_list = c1.split()
-    c2_list = c2.split()
+    result = None 
 
-    #remove or sign 
-    while 'v' in c1_list: c1_list.remove('v')    
-    while 'v' in c2_list: c2_list.remove('v')    
+    if len(c1) < len(c2):
+        c1, c2 = c2, c1
+    print('pl_resolve: ', c1, ', ', c2)
+
+    #if c2 is negative search for a positive in c1
+    if is_negated(c2) and ' ' + c2 + ' ' not in c1: #ovaj  2 uvjet je ako ima npr ~c i ~c da se ne vrati ~NIL
+        non_negated = negate_term(c2)
+        if ' ' + non_negated + ' ' in c1:
+            
+            placement_check_1 = ' v ' + non_negated + ' v '
+            placement_check_2 = ' v ' + non_negated
+            placement_check_3 = non_negated + ' v '
+            placement_check_4 = non_negated
+
+            if placement_check_1 in c1:
+                result = c1.replace(placement_check_1, ' v ')
+            elif placement_check_2 in c1:
+                result = c1.replace(placement_check_2, '')
+            elif placement_check_3 in c1:
+                result = c1.replace(placement_check_3, '')
+            elif placement_check_4 in c1:
+                result = c1.replace(placement_check_4, 'NIL')
+    #if c2 is positive search for a negative in c1
+    elif (not is_negated(c2) and ' ~' + c2 + ' ' in c1):
+        negated = negate_term(c2)
+        if negated in c1:
+            
+            placement_check_1 = ' v ' + negated + ' v '
+            placement_check_2 = ' v ' + negated
+            placement_check_3 = negated + ' v '
+            placement_check_4 = negated
+
+            if placement_check_1 in c1:
+                result = c1.replace(placement_check_1, ' v ')
+            elif placement_check_2 in c1:
+                result = c1.replace(placement_check_2, '')
+            elif placement_check_3 in c1:
+                result = c1.replace(placement_check_3, '')
+            elif placement_check_4 in c1:
+                result = c1.replace(placement_check_4, 'NIL')
 
 
-    clauses_to_be_resolved = c1_list + c2_list
-
-    number_of_clauses = len(clauses_to_be_resolved)
-
-    clauses_to_be_removed = []
-
-    for i in range(number_of_clauses):
-        for j in range(number_of_clauses):
-            #print(clauses_to_be_resolved[i], clauses_to_be_resolved[j])
-            if is_tauntology(clauses_to_be_resolved[i], clauses_to_be_resolved[j]):
-                if clauses_to_be_resolved[i] not in clauses_to_be_removed:
-                    clauses_to_be_removed.append(clauses_to_be_resolved[i])
-                if clauses_to_be_resolved[j] not in clauses_to_be_removed:
-                    clauses_to_be_removed.append(clauses_to_be_resolved[j])
-
-    for clause in clauses_to_be_removed:
-        if clause in clauses_to_be_resolved:
-            clauses_to_be_resolved.remove(clause)
-    #print(clauses_to_be_resolved)
-    #print(clauses_to_be_removed)
-    result = clauses_to_be_resolved
+    print('pl_resolve returning: ', result)
     return result
+    
+
 
 def pl_resolution(F: set, G: str):
     clauses = set(F) # creates a set of clauses
@@ -102,27 +125,31 @@ def pl_resolution(F: set, G: str):
     new_clauses.add(goal)
 
     
-    while True:
+    while new_clauses:
         print('#'*32)
         print('for loop: clauses= ' + str(clauses) + ', new_clauses= ' + str(new_clauses))
-        for [c1, c2] in select_clauses(clauses, new_clauses):
+        new_clause = new_clauses.pop()
+        print('new clause', new_clause)
+        for [c1, c2] in select_clauses(clauses, new_clause):
             resolvents = pl_resolve(c1, c2) #this should be a list since it requires a search
 
-            if 'NIL' in resolvents:
+            if resolvents != None and 'NIL' in resolvents:
                 conclusion = True
                 break
-            print(resolvents)
-
-
-            for resolvent in resolvents:
-                new_clauses.add(resolvent)
+            if resolvents != None:
+                #for resolvent in resolvents:
+                #    new_clauses.add(resolvent)
+                new_clauses.add(resolvents)
+        if conclusion:
+            break
 
         if new_clauses.issubset(clauses):
             conclusion = False
             break
 
-        clauses = clauses.add(new_clauses) 
-
+        clauses.add(new_clause) 
+    
+    print_conclusion(conclusion, G)
 def main():
     args = configure_parser_and_get_args()
 
